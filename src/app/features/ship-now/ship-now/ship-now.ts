899bedd37dd61ca040a7e07485e774b0515f616c
harikrishnan-ps <22cs346@mgits.ac.in>
2026-06-09 10:32:26 +0530

    feat: add ship-now feature

---
+++ src/app/features/ship-now/ship-now/ship-now.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ship-now',
+  imports: [],
+  templateUrl: './ship-now.html',
+  styleUrl: './ship-now.scss',
+})
+export class ShipNow {}
